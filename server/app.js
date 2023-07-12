a62675e19661e824b59528141ac79dcd9d841065
No Need <77963254+G8ur@users.noreply.github.com>
2023-07-12 16:34:55 +0530

    jsonwebtoken

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcryptjs = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 // connectin g to db
 require("./db/connection");
 
@@ -20,7 +21,7 @@ app.get("/", (req, res) => {
   res.end();
 });
 
-app.post("/api/register", async(req,res) =>{
+app.post("/api/register", async(req,res, next) =>{
 try {
   const { fullName, email, password } = req.body;
 
@@ -36,14 +37,67 @@ try {
 
         newUser.set('password', hashedPassword);
         newUser.save();
-        next();
+        next()
 
       })
       return res.status(200).send("User registered succsefully")
     }
   }
-} catch (error) {}
+} catch (error) {
+    console.log(error)
+}
 })
+
+
+app.post('/api/login', async(req, res, next) =>{
+    try{
+        const{email , password} = req.body;
+        if ( !email || !password) {
+            res.status(404).send("Plz fill all the required fields");
+          } else {
+            const user = await Users.findOne({email});
+            if(!user){
+                res.status(400).send("User or password is incorrect")
+
+            }
+            else{
+                const validatUser = await bcryptjs.compare(password , user.password)
+                if(!validatUser){
+                    res.status(400).send("User or email incoorect")
+
+                }
+
+                else{
+                    const payload ={
+                        userId:user._id,
+                        email: user.email
+                    }
+                    const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'THE_KEY_WHICH_CREATED'
+
+                    jwt.sign(payload, JWT_SECRET_KEY, {expiresIn:84600} , async(err, token) =>{
+                        await Users.updateOne({_id: user._id},{
+                            $set: {token}
+                        })
+                        user.save();
+                        next()
+
+
+                    })
+
+                    res.status(200).json({user:{email: user.email , fullName:user.fullName }, token: user.token})
+
+                }
+
+            }
+          }
+
+    }
+    catch(error){
+        console.log(error)
+
+    }
+})
+
 app.listen(port, () => {
   console.log("listenin on port" + port);
 });
